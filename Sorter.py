3a9a7e05c3f867faef776e4d5a62be5f31dbfc9c
knighthat <knightchanelgaming@gmail.com>
2023-10-11 18:52:45 -0500

    added copyright notice

--- Sorter.py
+++ Sorter.py
@@ -1,10 +1,32 @@
+#  Copyright (c) 2023. Knight Hat
+#  All rights reserved.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights to
+#  use,copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+#  of the Software, and to permit persons to whom the Software is furnished
+#  to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+#  WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+#  PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS
+#  OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+#  OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import os
 import re
 import shutil
-import PrettierLog as Log
-from MediaFile import Media
-from Enums import SortBy, SortOption
 
+import PrettierLog as Log
+from Enums import SortBy, SortOption
+from MediaFile import Media
 
 length_pattern = r'^\d{2}\:\d{2}\:\d{2}$'
 
@@ -44,9 +66,9 @@ def _sort_by_length(media: Media, option: SortOption, value: str) -> bool:
 
     hour, minute, second = value.split(":")
     if int(hour) > 0:
-        minute += int(hour) * 60     # Hour to minutes
+        minute += int(hour) * 60  # Hour to minutes
     if int(minute) > 0:
-        second += int(minute) * 60   # Minute to seconds
+        second += int(minute) * 60  # Minute to seconds
 
     result = False
     if option == SortOption.G:
